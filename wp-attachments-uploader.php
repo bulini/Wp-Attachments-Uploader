236b44f9c776a55adbd0e9744909b26c449f81e7
bulini <pinobulini@gmail.com>
2017-12-01 00:51:28 +0100

    sample data hook

--- wp-attachments-uploader.php
+++ wp-attachments-uploader.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name:     Wp Attachments Uploader
- * Plugin URI:      https://www.giuseppesurace.com
- * Description:     Useful metabox to upload each kind of attachments to your post and pages and custom post types.
- * Author:          bulini
- * Author URI:      https://www.giuseppesurace.com
- * Text Domain:     wp-attachments-uploader
- * Domain Path:     /languages
- * Version:         0.1.0
- *
- * @package         Wp_Attachments_Uploader
- */
+* Plugin Name:     Wp Attachments Uploader
+* Plugin URI:      https://www.giuseppesurace.com
+* Description:     Useful metabox to upload each kind of attachments to your post and pages and custom post types.
+* Author:          bulini
+* Author URI:      https://www.giuseppesurace.com
+* Text Domain:     wp-attachments-uploader
+* Domain Path:     /languages
+* Version:         0.1.0
+*
+* @package         Wp_Attachments_Uploader
+*/
 
 
 define('UPLOADER_ABSOLUTE_URL', plugin_dir_url( __FILE__ ));
@@ -18,3 +18,5 @@ define('UPLOADER_ABSOLUTE_URL', plugin_dir_url( __FILE__ ));
 load_plugin_textdomain('wp-attachments-uploader', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
 require_once('lib/class-multiple-uploader.php');
+
+register_activation_hook( __FILE__, array( 'MultipleUploader', 'multiple_uploader_activation' ) );
